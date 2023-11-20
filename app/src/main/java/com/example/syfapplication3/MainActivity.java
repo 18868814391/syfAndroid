472dd71a234c7f1f295b77d907aca9acbd29fc7e
pptvvx <605729908@qq.com>
2023-11-20 16:28:36 +0800

    mycommit

---
+++ app/src/main/java/com/example/syfapplication3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.syfapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    int size=30;
+    public void bigger(View v){     // 按钮对应的 onclick 响应
+        TextView txv;
+        txv= (TextView) findViewById(R.id.txv);  // 根据ID找到对应的text对象
+        txv.setTextSize(++size);       // 修改对象的字符大小-size
+    }
+    public void display(View v){     // 另外一个按钮对应的 onclick 响应
+        EditText name= (EditText) findViewById(R.id.name);  //还是根据ID找到对象，并进行接下来的操作
+        TextView text2= (TextView) findViewById(R.id.txv);
+
+        text2.setText(name.getText().toString());   // 设置字符
+    }
+}
